69c7cf89efae1cb9240cf1c9fa2716e8bc5a10a3
Vasken Houdoverdov <vhcomptech@gmail.com>
2018-02-03 16:21:26 -0800

    LR heartbeat util

---
+++ LogRhythm/Get-HeartbeatOffset.ps1
@@ -0,0 +1,15 @@
+﻿Function Get-HeartBeatOffset {
+Param([string]$LogFile)
+
+if (!$LogFile) {throw 'Specify a path to a LogRhythm mediator log using the -LogFile parameter'}
+
+$Hosts = @();
+
+gc $LogFile | % { 
+if($_ -match 'from (.+): clock offset = (.+) minutes$') { 
+    $Properties = @{Host =   $Matches[1]; Offset = $Matches[2]}
+    $Hosts += New-Object Psobject -Property $Properties}}
+$High =  [Double]+0.99
+$Low =   [Double]-0.99
+
+$Hosts | Get-Unique -AsString  | % {if([Double]$_.Offset -gt $High -or[Double]$_.Offset -lt $Low){$_}}}
